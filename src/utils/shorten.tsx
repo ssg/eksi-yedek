6dd01ce457b596f4803f4a5c4536cbc25e9107e1
skacurt <skacurt@outlook.com>
2026-06-16 04:20:13 +0300

    fix shortenUrlDisplay to shorten URLs correctly

--- src/utils/shorten.tsx
+++ src/utils/shorten.tsx
@@ -1,16 +1,18 @@
 // Makes the relative path portion of the given URL suitable for display when it's too long.
 // Returns a string that trims the beginning of the relative path and shows a trailing portion of it.
 export function shortenUrlDisplay(url: string): string {
-    const maxRelativeLength = 30
-    try {
-        const { origin, pathname, search, hash } = new URL(url)
-        const relativeUrl = pathname + search + hash
-        const relativeUrlNoPrefix = relativeUrl.slice(1)
-        if (relativeUrlNoPrefix.length > maxRelativeLength) {
-            const lastPart = relativeUrlNoPrefix.slice(-maxRelativeLength)
-            return `${origin}/…${lastPart}`
-        }
+    const maxUrlLength = 50;
+
+    if (url.length <= maxUrlLength) {
         return url
+    }
+
+    try {
+        const { origin } = new URL(url);
+        const baseUrl = origin + "/";
+        const shortenLength = maxUrlLength - baseUrl.length;
+        const shortened = shortenLength > 0 ? url.slice(-shortenLength) : "";
+        return `${baseUrl}…${shortened}`;
     } catch {
         return url
     }
--- test/shorten.test.mjs
+++ test/shorten.test.mjs
@@ -16,7 +16,27 @@ describe('Shorten Url Display', () => {
             name: 'long URL',
             input: 'https://example.com/otoban-kenarinda-ebegumeci-toplayan-yurdumun-cefak--160792',
             expected: 'https://example.com/…oplayan-yurdumun-cefak--160792'
-        },        
+        },
+        {
+            name: 'long URL with port',
+            input: 'https://example.com:3232/otoban-kenarinda-ebegumeci-toplayan-yurdumun-cefak--160792',
+            expected: 'https://example.com:3232/…an-yurdumun-cefak--160792'
+        },
+        {
+            name: 'long URL + long hostname',
+            input: 'https://example.example.example.com/otoban-kenarinda-ebegumeci-toplayan-yurdumun-cefak--160792',
+            expected: 'https://example.example.example.com/…-cefak--160792'
+        },
+        {
+            name: 'long URL + long hostname + port',
+            input: 'https://example.example.example.com:8080/otoban-kenarinda-ebegumeci-toplayan-yurdumun-cefak--160792',
+            expected: 'https://example.example.example.com:8080/…k--160792'
+        },
+        {
+            name: 'long URL with very long hostname',
+            input: 'https://example.example.example.example.example.com/otoban-kenarinda-ebegumeci-toplayan-yurdumun-cefak--160792',
+            expected: 'https://example.example.example.example.example.com/…'
+        },
         {
             name: 'invalid URL',
             input: 'http://|',
